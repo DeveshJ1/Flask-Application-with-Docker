7a28404cfa971df9e667fe325d0914f65b5975e5
Devesh Jaiswal <Devesh@Mac.lan>
2025-07-16 18:19:59 -0400

    final version

---
+++ run.py
@@ -0,0 +1,19 @@
+from flask import Flask
+from datetime import datetime
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello world!'
+
+@app.route('/time')
+def current_time():
+	now=datetime.now()
+	current_time = now.strftime("%Y-%m-%d %H:%M:%S")
+	return 'Current time accessed is : {}'.format(current_time)
+
+app.run(host='0.0.0.0',
+        port=8080,
+        debug=True)
